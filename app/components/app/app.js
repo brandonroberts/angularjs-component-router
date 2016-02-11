7b43b822d03a72f7adb106a16dcfb5313f8551d5
Brandon Roberts <robertsbt@gmail.com>
2016-02-10 19:57:38 -0600

    refactor(routes): Moved routes into root component $routeConfig

--- app/components/app/app.js
+++ app/components/app/app.js
@@ -10,37 +10,14 @@ var app = angular.module('myApp', [
 	'app.404'
 ]);
 
+app.value('$routerRootComponent', 'app');
 app.component('app', {
 	templateUrl: 'components/app/app.html',
-	controller: ['$router', 'Auth', AppComponentController]
+	$routeConfig: [
+		{ path: '/', component: 'home', name: 'Home' },
+		{ path: '/about/:name', component: 'about', name: 'About' },
+		{ path: '/login', component: 'login', name: 'Login' },
+		{ path: '/admin/...', component: 'admin', name: 'Admin' },
+		{ path: '/**', component: 'notfound', name: 'NotFound' }
+	]
 });
-
-function AppComponentController($router) {
-	$router.config([
-		{
-			path: '/',
-			component: 'home',
-			name: 'Home'
-		},
-		{
-			path: '/about/:name',
-			component: 'about',
-			name: 'About'
-		},
-		{
-			path: '/login',
-			component: 'login',
-			name: 'Login'
-		},
-		{
-			path: '/admin/...',
-			component: 'admin',
-			name: 'Admin'
-		},
-		{
-			path: '/**',
-			component: 'notfound',
-			name: 'NotFound'
-		}
-	]);
-}
