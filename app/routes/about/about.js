20e90dd8b0329b79b843b2c603d7710a40a5047b
Brandon Roberts <robertsbt@gmail.com>
2016-02-10 20:06:16 -0600

    fix(route): Fixed lifecycle hook in about route

--- app/routes/about/about.js
+++ app/routes/about/about.js
@@ -1,7 +1,11 @@
 angular.module('app.about', []).component('about', {
   template: 'About {{ vm.name }}',
   controllerAs: 'vm',
-  $routerOnActivate: function(toRoute, fromRoute) {
-  	this.name = toRoute.params.name;
+  controller: function() {
+    var vm = this;
+
+    vm.$routerOnActivate = function(toRoute, fromRoute) {
+    	this.name = toRoute.params.name;
+    };
   }
 });
